abcfd45231024082eb3244a73ea60d10e73e5644
Alexandre DUARTE <alexandre.jose.duarte@gmail.com>
2024-07-12 11:27:41 +0200

    add card view happy flow

--- OnePieceTCG/OnePieceTCG/Application/CollectionDetails/Views/CardView.swift
+++ OnePieceTCG/OnePieceTCG/Application/CollectionDetails/Views/CardView.swift
@@ -11,15 +11,23 @@ struct CardView: View {
     let url = URL(string: "https://static.opecards.fr/cards/en/op01/image-trading-cards-one-piece-card-game-tcg-opecards-en-op01-001-l-roronoa-zoro.webp")
     
     var body: some View {
-        GeometryReader { geometry in
-            AsyncImage(url: url) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                } placeholder: {
-                    ProgressView()
-                }
-                .frame(width: geometry.size.width * 0.45)
+        AsyncImage(
+            url: url,
+            transaction: Transaction(animation: .linear)
+        ) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+            case .failure:
+                Image(systemName: "wifi.slash")
+                //Customized failure image
+            @unknown default:
+                EmptyView()
+            }
         }
     }
 }
--- OnePieceTCG/OnePieceTCG/Application/CollectionDetails/Views/CollectionDetailsView.swift
+++ OnePieceTCG/OnePieceTCG/Application/CollectionDetails/Views/CollectionDetailsView.swift
@@ -10,12 +10,23 @@ import SwiftUI
 struct CollectionDetailsView: View {
     let collection: Collection
     
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
         let _ = Self._printChanges()
         ScrollView {
             MetricsHeaderView(collection: collection)
                 .padding(.horizontal, .md)
                 .navigationTitle(collection.shortName)
+            LazyVGrid(columns: columns, spacing: .sm) {
+                CardView()
+                CardView()
+            }
+            .padding(.vertical, .sm)
+            .padding(.horizontal, .md)
         }
     }
 }
